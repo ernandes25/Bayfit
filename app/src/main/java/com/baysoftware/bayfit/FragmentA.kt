876ad14b8f407994ab763814a3734f1b6d13a71c
Higor Ernandes <higorernandes@gmail.com>
2023-12-18 20:10:39 -0300

    Ajustes de layout e navegação.

--- app/src/main/java/com/baysoftware/bayfit/FragmentA.kt
+++ app/src/main/java/com/baysoftware/bayfit/FragmentA.kt
@@ -1,48 +1,31 @@
 package com.baysoftware.bayfit
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-
-import androidx.navigation.Navigation
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.baysoftware.bayfit.databinding.FragmentABinding
 
 class FragmentA : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var binding: FragmentABinding
 
     override fun onCreateView(
-          inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_a, container, false)
-
-        view.findViewById<ImageButton>(R.id.imageButtonIniciar).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_fragmentA_to_fragmentB)
-        }
-        return view
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_a, container, false)
+        return binding.root
     }
 
-    companion object {
-
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentA().apply {
-                arguments = Bundle().apply {
-
-
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageButtonIniciar.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentA_to_fragmentB)
+        }
     }
 }
