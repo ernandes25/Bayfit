cac72efba9a4ae73f919467a665de01d93b227f3
ernandes25 <antonioernandes25@gmail.com>
2024-04-27 09:31:16 -0300

    ajustes pull request

--- app/src/main/java/com/baysoftware/bayfit/ExerciseSession.kt
+++ app/src/main/java/com/baysoftware/bayfit/ExerciseSession.kt
@@ -1,15 +1,14 @@
 package com.baysoftware.bayfit
 
 
-
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "history_table")
-data class ExerciseSession(@PrimaryKey @ColumnInfo(name = "data")
-                           val data: String,
-                           val duration : Int,
-                           val totalTimePause : Int
+data class ExerciseSession(
+    @PrimaryKey @ColumnInfo(name = "data")
+    val data: String,
+    val duration: Int,
+    val totalTimePause: Int
 )
